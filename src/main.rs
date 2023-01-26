102708a483026dc6c7d0cca417f22535f939993e
Emre <aka548548@gmail.com>
2023-01-26 23:48:10 +0300

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    //heap allocated!
+    let mut name = String::new();
+    let greeting = "Nice to meet you!";
+    //recieve input and if cant get any input panic! with given message.
+    io::stdin()
+    .read_line(&mut name)
+    .expect("Didn't receive input!");
+    
+    println!("Hello {}! {}", name.trim_end(), greeting)
 }
