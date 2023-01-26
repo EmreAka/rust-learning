fd1ad99189fcb6ebdfda5c13dcf27921e07d0b94
Emre <aka548548@gmail.com>
2023-01-26 23:57:38 +0300

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,12 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
+    const ONE_MIL: u32 = 1_000_00;
+    const PI: f32 = 3.141592;
+
+    //shadowing -> same names dif data types
+    let age = "47";
+    let mut age: u32 = age.trim().parse().expect("Age wasn't assigned a number!");
+    age = age + 1;
+    println!("I'm {} and I want ${}", age, ONE_MIL);
 }
