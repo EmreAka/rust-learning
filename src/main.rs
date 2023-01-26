35cc3956dee356746215ba5c6549ed87e03e0b62
Emre <aka548548@gmail.com>
2023-01-27 01:01:55 +0300

    casting

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,9 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
+    let int_u8: u8 = 5;
+    let int2_u8 = 4;
+
+    //casting
+    let int3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
 }
