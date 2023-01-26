8d762b65a09162e19cd99fe1a43c36f62822dcc4
Emre <aka548548@gmail.com>
2023-01-27 00:24:44 +0300

    match

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,12 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
+    let my_age = 23;
+    let voting_age = 18;
+
+    match my_age.cmp(&voting_age) {
+        Ordering::Less => println!("Can't vote"),
+        Ordering::Greater => println!("Can vote"),
+        Ordering::Equal => print!("You gained the right to vote!"),
+    }
 }
