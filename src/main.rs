9c659e0721cb106d4783740219fdcf72b5912780
Emre AKA <aka548548@gmail.com>
2023-11-24 18:45:05 +0300

    read input

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,12 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("What is your name?");
+
     //heap allocated!
     let mut name = String::new();
+
     let greeting = "Nice to meet you!";
+    
     //recieve input and if cant get any input panic! with given message.
     io::stdin()
     .read_line(&mut name)
