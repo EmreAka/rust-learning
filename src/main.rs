5cd75a715466e9e4ca90d8c8c525401a0bba54aa
Emre AKA <aka548548@gmail.com>
2023-11-25 16:28:33 +0300

    binary search

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,31 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
+    let numbers = vec![1,2,3,4,5,6,7,8,10];
+
+    let index = binary_search(8, numbers);
+    println!("{}", index);
+}
+
+fn binary_search(search_number: i32, numbers: Vec<i32>) -> i32 {
+    let mut left = 0;
+    let mut right = (numbers.len() - 1) as i32;
+
+    while (left <= right) {
+        let mid = (left + (right - left / 2)) as usize;
+
+        if (search_number == numbers[mid]) {
+            return mid as i32;
+        }
+
+        else if (search_number > numbers[mid]) {
+            left = (mid + 1) as i32;
+        }
+
+        else {
+            right = (mid - 1) as i32;
+        }
+    }
+
+    return -1;
 }
