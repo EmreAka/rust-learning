e1c65c4e0a970649600382b5657d358ae20ef46c
Emre AKA <aka548548@gmail.com>
2023-11-29 21:46:11 +0300

    generics

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-#![allow(unused)]
 
-use std::io;
-use rand::Rng;
-use std::io::{Write, BufRead, BufReader, ErrorKind};
-use std::fs::File;
-use std::cmp::Ordering;
+// T is a type parameter name.
+// We can only use types whose values can be ordered. 
+
+// Monomorphization: Rust compiler implements the generic method by all 
+// types it is used with throughout the program.
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest: &T = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
 
 fn main() {
-    println!("Hello, world!");
+    let number_list: Vec<i32> = vec![34, 50, 2500, 100, 65];
+    let result: &i32 = largest(&number_list);
+    println!("Largest number is: {}", result);
 }
